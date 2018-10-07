146669040ddeec401329846114a91557a454da81
Juan Arias <juan.arias@marist.edu>
2018-10-07 17:54:34 -0400

    Feedback

--- labs /5/Lab5.java
+++ labs /5/Lab5.java
@@ -5,68 +5,43 @@ public class Lab5 {
 
 	public static void main(String[] args) {
 
-	int size;
+		int size;
 
-	// Input size of the matrix
+		// Input size of the matrix
 
-	Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
-	System.out.println("Enter the size of matrix");
+		System.out.println("Enter the size of matrix");
 
-	size = sc.nextInt();
+		size = sc.nextInt();
 
-	  
+		int mat[][]=new int[size][size];
 
-	int mat[][]=new int[size][size];
+		System.out.println("Enter numbers into the matrix");
 
-	System.out.println("Enter numbers into the matrix");
+		for(int i=0;i<3;i++){   
+			for(int j=0;j<3;j++){
+				mat[i][j] = sc.nextInt();  
+			}
+		}
 
-	for(int i=0;i<3;i++){   
+		System.out.println("Original matrix");
 
-	for(int j=0;j<3;j++){
+		for(int i=0;i<size;i++){   
+			for(int j=0;j<size;j++){
+				System.out.print(mat[i][j] + " ");
+			}
+			System.out.println();
+		}
 
-	mat[i][j] = sc.nextInt();  
+		System.out.println("Transpose of the matrix");
 
-	}
-
-	}
-
-	  
-
-	System.out.println("Original matrix");
-
-	for(int i=0;i<size;i++){   
-
-	for(int j=0;j<size;j++){
-
-	System.out.print(mat[i][j] + " ");
-
-	}
-
-	System.out.println();
-
-	}
-
-	  
-
-	System.out.println("Transpose of the matrix");
-
-	for(int i=0;i<size;i++){   
-
-	for(int j=0;j<size;j++){
-
-	System.out.print(mat[j][i] + " ");
-
-	}
-
-	System.out.println();
-
-	}
-
-	  
-
-	  
-
+		for(int i=0;i<size;i++){   
+			for(int j=0;j<size;j++){
+				System.out.print(mat[j][i] + " ");
+			}
+			System.out.println();
+		}
 	}
 }
 
