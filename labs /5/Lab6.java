338fea4a9e608f9c8f9ab2e49b770d40702ac826
Juan Arias <juan.arias@marist.edu>
2018-10-07 17:59:04 -0400

    Feedback

--- labs /5/Lab6.java
+++ labs /5/Lab6.java
@@ -5,81 +5,51 @@ import java.util.Scanner;
 public class Lab6 {
 
 	public static double max(double[] array) {
-
-	double maximum = array[0];
-
-	for (int i = 0; i < array.length; i++)
-
-	if (array[i] > maximum)
-
-	maximum = array[i];
-
-	return maximum;
-
+		double maximum = array[0];
+		for (int i = 0; i < array.length; i++)
+			if (array[i] > maximum)
+				maximum = array[i];
+		return maximum;
 	}
 
 	public static double averageRow(double[][] array, int columnIndex){
 
-	double sum = 0;
+		double sum = 0;
+		// print the 2D array
+		System.out.println("The Array is:------>");
+		for(int i=0;i<5; i++){
+			for(int j=0; j<5; j++){
+				System.out.print(array[i][j] + "\t\t");
+			}
+			System.out.println();
+		}
 
-	// print the 2D array
-
-	System.out.println("The Array is:------>");
-
-	for(int i=0;i<5; i++){
-
-	for(int j=0; j<5; j++){
-
-	System.out.print(array[i][j] + "\t\t");
-
-	}
-
-	System.out.println();
-
-	}
-
-	for(int i = 0; i <5; i++){
-
-	sum += array[i][columnIndex];
-
-	}
-
-	return (sum/5);
-
+		for(int i = 0; i < array.length; i++){
+			sum += array[i][columnIndex];
+		}
+		return (sum / array.length);
 	}
 
 	public static void main(String[] args) {
 
-	Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 10 floating point numbers: ");
+		double array[] = new double[10];
+		for (int i = 0; i < 10; i++) {
+			array[i] = Integer.parseInt(sc.nextLine());
+		}
+		System.out.println("Maximum element in the array is: " + max(array));
 
-	System.out.println("Enter 10 floating point numbers: ");
+		// Here I am considering 5*5 2D array
 
-	double array[] = new double[10];
+		double[][] twoDimArray = {{1, 2, 3, 4, 5},
+			{6, 7, 8, 9, 10},
+			{11, 12, 13, 14, 15},
+			{16, 17, 18, 19, 20},
+			{21, 22, 23, 24, 25},
+		};
 
-	for (int i = 0; i < 10; i++) {
-
-	array[i] = Integer.parseInt(sc.nextLine());
-
+		System.out.println("Average of 4th column values is: " + averageRow(twoDimArray, 3)); // here 3 means 4th column
 	}
 
-	System.out.println("Maximum element in the array is: " + max(array));
-
-	// Here I am considering 5*5 2D array
-
-	double[][] twoDimArray = {{1, 2, 3, 4, 5},
-
-	{6, 7, 8, 9, 10},
-
-	{11, 12, 13, 14, 15},
-
-	{16, 17, 18, 19, 20},
-
-	{21, 22, 23, 24, 25},
-
-	};
-
-	System.out.println("Average of 4th column values is: " + averageRow(twoDimArray, 3)); // here 3 means 4th column
-
-	}
-
-	}
+}
